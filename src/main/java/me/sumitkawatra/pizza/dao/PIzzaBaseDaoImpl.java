2457e9c5d7b933bbe0cecfa29a8b47aae586a60c
Sumit Kawatra <sumitkkawatra@gmail.com>
2017-01-04 01:09:09 +0530

    Pizza Base Dao Impl done

--- src/main/java/me/sumitkawatra/pizza/dao/PIzzaBaseDaoImpl.java
+++ src/main/java/me/sumitkawatra/pizza/dao/PIzzaBaseDaoImpl.java
@@ -3,7 +3,17 @@
  */
 package me.sumitkawatra.pizza.dao;
 
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import me.sumitkawatra.pizza.domain.Base;
 
 /**
  * @author sumitkawatra
@@ -12,11 +22,66 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class PIzzaBaseDaoImpl implements PizzaBaseDao {
 
+	@Autowired
+	private SessionFactory sessionFactory;
+
 	/**
 	 * 
 	 */
 	public PIzzaBaseDaoImpl() {
 
+	}
+
+	public SessionFactory getSessionFactory() {
+		return sessionFactory;
+	}
+
+	public void setSessionFactory(SessionFactory sessionFactory) {
+		this.sessionFactory = sessionFactory;
+	}
+
+	public Base savePizzaBase(Base pizzaBase) {
+		Session session = sessionFactory.openSession();
+		session.beginTransaction();
+		Long pizzaBaseId = (Long) session.save(pizzaBase);
+		session.flush();
+		session.getTransaction().commit();
+		session.close();
+		return getPizzaBase(pizzaBaseId);
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<Base> listBase() {
+		List<Base> pizzaBases = new ArrayList<Base>();
+		Session session = sessionFactory.openSession();
+		session.beginTransaction();
+		Query query = session.createQuery("from Base");
+		pizzaBases = query.list();
+		session.getTransaction().commit();
+		session.close();
+		return pizzaBases;
+	}
+
+	public Base getPizzaBase(Long pizzaBaseId) {
+		Base pizzaBase = new Base();
+		Session session = sessionFactory.openSession();
+		session.beginTransaction();
+		pizzaBase = (Base) session.get(Base.class, pizzaBaseId);
+		session.getTransaction().commit();
+		session.close();
+		return pizzaBase;
+	}
+
+	public Base getByName(String pizzaBaseName) {
+		Base pizzaBase = new Base();
+		Session session = sessionFactory.openSession();
+		session.beginTransaction();
+		Query query = session.createQuery("from Base where name = :pizzaBaseName");
+		query.setString("pizzaBaseName", pizzaBaseName);
+		pizzaBase = (Base) query.uniqueResult();
+		session.getTransaction().commit();
+		session.close();
+		return pizzaBase;
 	}
 
 }
--- src/main/java/me/sumitkawatra/pizza/dao/PizzaBaseDao.java
+++ src/main/java/me/sumitkawatra/pizza/dao/PizzaBaseDao.java
@@ -3,10 +3,41 @@
  */
 package me.sumitkawatra.pizza.dao;
 
+import java.util.List;
+
+import me.sumitkawatra.pizza.domain.Base;
+
 /**
  * @author sumitkawatra
  *
  */
 public interface PizzaBaseDao {
+	
+	/**
+	 * save Pizza Base
+	 * @param pizzaBase
+	 * @return Base
+	 */
+	public abstract Base savePizzaBase(Base pizzaBase);
+	
+	/**
+	 * get list of Pizza Base.
+	 * @return List<Base>
+	 */
+	public abstract List<Base> listBase();
+	
+	/**
+	 * get Pizza Base per pizzaBaseId
+	 * @param pizzBaseId
+	 * @return Base
+	 */
+	public abstract Base getPizzaBase(Long pizzBaseId); 
+	
+	/**
+	 * get Pizza Base by Name
+	 * @param pizzaBaseName
+	 * @return Base
+	 */
+	public abstract Base getByName(String pizzaBaseName);
 
 }
