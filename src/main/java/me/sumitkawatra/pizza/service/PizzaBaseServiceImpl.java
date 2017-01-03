738884e5e590141c708acff5eefaaeff3d2c901a
Sumit Kawatra <sumitkkawatra@gmail.com>
2017-01-04 01:10:29 +0530

    Pizza Base Service Impl code.

--- src/main/java/me/sumitkawatra/pizza/service/PizzaBaseServiceImpl.java
+++ src/main/java/me/sumitkawatra/pizza/service/PizzaBaseServiceImpl.java
@@ -3,10 +3,13 @@
  */
 package me.sumitkawatra.pizza.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import me.sumitkawatra.pizza.dao.PizzaBaseDao;
+import me.sumitkawatra.pizza.domain.Base;
 
 /**
  * @author sumitkawatra
@@ -28,6 +31,22 @@ public class PizzaBaseServiceImpl implements PizzaBaseService {
 
 	public void setPizzaBaseDao(PizzaBaseDao pizzaBaseDao) {
 		this.pizzaBaseDao = pizzaBaseDao;
+	}
+
+	public Base savePizzaBase(Base pizzaBase) {
+		return pizzaBaseDao.savePizzaBase(pizzaBase);
+	}
+
+	public List<Base> listBase() {
+		return pizzaBaseDao.listBase();
+	}
+
+	public Base getPizzaBase(Long pizzBaseId) {
+		return pizzaBaseDao.getPizzaBase(pizzBaseId);
+	}
+
+	public Base getByName(String pizzaBaseName) {
+		return pizzaBaseDao.getByName(pizzaBaseName);
 	}
 
 }
